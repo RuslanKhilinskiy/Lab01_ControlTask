ea599fa21a1a078157531dc35ccf88d50438142c
Ruslan <ruslan.hilinsky@yandex.ru>
2023-11-20 02:25:49 +0300

    Упражнение 1-5 + Контрольное задание Лабораторной работы №1

---
+++ Control Task 01/trianglePerimeter/trianglePerimeter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trianglePerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter perimiter value:");
+                double perimiter = double.Parse(Console.ReadLine());
+
+                double side = perimiter / 3;
+                double semiPerimiter = perimiter / 2;
+                double S = Math.Sqrt(semiPerimiter * (semiPerimiter - side) * (semiPerimiter - side) * (semiPerimiter - side));
+                Console.WriteLine("Сторона\t\tПлощадь");
+                Console.WriteLine($"{side}\t\t{S:F2}");
+            }
+            catch (FormatException e) {
+                Console.WriteLine("An format exception was thrown: {0}", e.Message);
+            }
+            catch (Exception e) {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+        }
+    }
+}
